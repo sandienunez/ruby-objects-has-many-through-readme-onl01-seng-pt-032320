e07dc719983396a70801d52bcffdedb589932f03
github username <github email address>
2020-04-30 14:48:05 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,34 @@
-class Customer
-  
+require "pry"
+class Customer 
+    attr_accessor :name, :age 
+    @@all = [] 
+    
+    def initialize(name, age)
+        @name = name
+        @age = age 
+        @@all << self 
+    end
+
+    def self.all
+        @@all 
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+    def meals #returns an array of only our meals
+      #Meals.all returns all the meals ever not just our meals
+        Meal.all.select do |meal_obj|
+            meal_obj.customer == self  #not the class /selecting whatever current customer
+        end 
+    end 
+
+    def waiters # asking what the waiter is for each meal and returns it
+      # binding.pry
+      # we are calling meals method to find out our meals
+      #then we go through our meals and ask who the waiter is
+        meals.map do |meal_obj|
+            meal_obj.waiter 
+       end
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,32 @@
 class Waiter
+    attr_accessor :name, :yrs_experience
+    @@all = []
+    def initialize(name, yrs_experience)
+        @name = name 
+        @yrs_experience = yrs_experience
+        @@all << self 
+    end 
+    def self.all 
+        @@all 
+    end
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
 
+    def meals #returns an array of only our meals
+        #Meals.all returns all the meals ever not just our meals
+          Meal.all.select do |meal_obj|
+              meal_obj.waiter == self  #not the class /selecting whatever current customer
+          end 
+      end 
+      def best_tipper #return the customer who tips the best
+        best_tipped_meal = meals.max do |meal_a, meal_b|
+          meal_a.tip <=> meal_b.tip
+        end
+        #sets variable best tipped meal to the object with the highest tip
+        #returns that object
+      
+        best_tipped_meal.customer
+        #returns the object with the best tipped meal's customer
+      end
 end
